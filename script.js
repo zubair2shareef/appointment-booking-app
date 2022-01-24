b1469c57a39517e4638a32bce28d2760644ed2b8
zubair1shareef <66367807+zubair1shareef@users.noreply.github.com>
2022-01-24 22:10:57 +0530

    object to local storage

--- script.js
+++ script.js
@@ -5,11 +5,22 @@ console.log(form)
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
 
+
+    let userDetails={
+        userName:e.target.name.value,
+        userEmail:e.target.email.value,
+        userPhoneno:e.target.phoneno.value,
+        userDateTime:e.target.datetime.value
+
+    };
+    console.log(userDetails);
+    localStorage.setItem('userData',JSON.stringify(userDetails));
+
     
-    localStorage.setItem("userName",e.target.name.value);
-    localStorage.setItem("userEmail",e.target.email.value);
-    localStorage.setItem("userPhoneNo",JSON.stringify(e.target.phoneno.value.toString));
-    localStorage.setItem("userDateTime",e.target.datetime.value);
+    // localStorage.setItem("userName",e.target.name.value);
+    // localStorage.setItem("userEmail",e.target.email.value);
+    // localStorage.setItem("userPhoneNo",JSON.stringify(e.target.phoneno.value.toString));
+    // localStorage.setItem("userDateTime",e.target.datetime.value);
 
     
   
@@ -29,3 +40,19 @@ form.addEventListener('submit', (e)=>{
     
 
 // }
+
+let show=document.querySelector('.showdetails')
+
+show.classList.contains("Name").innerHTML="fsdf"
+let saaa=show.getElementsByTagName("li");
+
+
+//showing data on frontend
+
+
+var getData=JSON.parse(localStorage.getItem('userData'))
+
+saaa.Name.innerHTML=getData.userName
+saaa.Email.innerHTML=getData.userEmail
+saaa.Phoneno.innerHTML=getData.userPhoneno
+saaa.Date.innerHTML=getData.userDateTime
